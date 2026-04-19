3199a1faf5794b8f43784e585c1ce85936217cec
Cross <crossd@vlsi55.bu.edu>
2026-04-19 15:33:10 -0400

    added header and created all activation weight and bias arrays/matrices

--- cereal.c
+++ cereal.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <math.h>
 
+#include "cereal.h"
+
 #define I_SIZE 784
 #define H0_SIZE 16
 #define H1_SIZE 16
@@ -10,38 +12,6 @@
 
 #define INIT_LOW 0
 #define INIT_HIGH 10
-
-typedef struct {
-  long int rowlen;
-  long int collen;
-  data_t *data;
-} matrix_rec, *matrix_ptr;
-
-typedef struct {
-  long int len;
-  data_t *data;
-} array_rec, *array_ptr;
-
-
-// array func declarations
-array_ptr new_array(long int len);
-int get_array_element(array_ptr v, long int index, data_t *dest);
-long int get_array_length(array_ptr v);
-int set_array_length(array_ptr v, long int index);
-int init_array(array_ptr v, long int len);
-
-// matrix func declarations
-double fRand(double fMin, double fMax);
-matrix_ptr new_matrix(long int len);
-int set_matrix_rowlen(matrix_ptr m, long int index);
-long int get_matrix_rowlen(matrix_ptr m);
-int init_matrix(matrix_ptr m, long int len);
-int zero_matrix(matrix_ptr m, long int len);
-int init_matrix_rand(matrix_ptr m, long int rowlen);
-int init_matrix_rand_ptr(matrix_ptr m, long int rowlen);
-
-
-
 
 
 int main() {
@@ -51,25 +21,22 @@ int main() {
     
     // H0 ARRAY, WEIGHTS, and BIASES
     array_ptr H0 = new_array(H0_SIZE);
-    matrix_ptr H0_W = new_matrix(H0_SIZE)
+    matrix_ptr H0_W = new_matrix(H0_SIZE, I_SIZE);
+    array_ptr H0_B = new_array(H0_SIZE);
 
     // H1 ARRAY
     array_ptr H1 = new_array(H1_SIZE);
-
+    matrix_ptr H1_W = new_matrix(H1_SIZE, H0_SIZE);
+    array_ptr H1_B = new_array(H1_SIZE);
 
     // OUTPUT ARRAY
     array_ptr output = new_array(L_SIZE);
-
+    matrix_ptr H0_W = new_matrix(H0_SIZE, I_SIZE);
+    array_ptr H0_B = new_array(H0_SIZE);
 
 
     return 0;
 }
-
-
-
-
-
-
 
 
 // MATRIX STRUCT FUNCTIONS ----- BORROWED FROM PROF. HERBORDT
